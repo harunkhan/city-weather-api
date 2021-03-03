ee9be00312e4847fffefe302a3923afd296ed5a0
harunkhan <harun.khan01@gmail.com>
2021-03-03 13:00:05 +0530

    Add files via upload

---
+++ CityInfo.API/Controllers/CityController.cs
@@ -0,0 +1,84 @@
+﻿using CityInfo.API.Model;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+//TODO:
+// 1. Error handling Not implemented.
+// 2. Need to test Put.
+// 3. tasks are Syncronus.
+// 4. Weather details are constant, need to create and actual AppId for realtime data.
+// 5. Solution can be improved further.
+
+/// <summary>
+/// This API provide CRUD operations for City.
+/// while getting the data, Along with City It Combines data for country and Weather.
+/// </summary>
+
+namespace CityInfo.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CityController : ControllerBase
+	{
+		private readonly ICityService _cityService;
+
+		public CityController(ICityService cityService) {
+			this._cityService = cityService;
+		}
+
+		// GET: api/<CityController>
+		[HttpGet]
+		public IActionResult Get() {
+			var cities = _cityService.GetCity();
+
+			if (cities.Any()) {
+				return Ok(cities);
+			}
+			return NoContent();
+		}
+
+		// GET api/<CityController>/Delhi
+		[HttpGet("{name}")]
+		public IActionResult Get(string name) {
+			var city = _cityService.GetCity(name);
+
+			if (city != null) {
+				return Ok(city);
+			}
+			return NotFound();
+		}
+
+		// POST api/<CityController>
+		[HttpPost]
+		public IActionResult Post([FromBody] CityModel city) {
+			if (ModelState.IsValid) {
+				var id = _cityService.AddCity(city);
+				return Ok(id);
+			}
+			return BadRequest(ModelState);
+		}
+
+		// PUT api/<CityController>/5
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] CityModel city) {
+			if (id > 0 && ModelState.IsValid) {
+				bool response = _cityService.UpdateCity(id, city);
+				return Ok(response);
+			}
+			return BadRequest(ModelState);
+		}
+
+		// DELETE api/<CityController>/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id) {
+			if (id > 0) {
+				var response = _cityService.DeleteCity(id);
+
+				return Ok(response);
+			}
+			return NotFound(id);
+		}
+	}
+}
---
+++ CityInfo.API/Model/City.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Model
+{
+	public class City
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public string State { get; set; }
+
+		public string Country { get; set; }
+
+		public int Rating { get; set; }
+
+		public DateTime EstablishedOn { get; set; }
+
+		public long Population { get; set; }
+
+		public string CountryCode2 { get; set; }
+
+		public string CountryCode3 { get; set; }
+
+		public string Currency { get; set; }
+
+		public CountryDetails CountryDetails { get; set; }
+
+		public WeatherDetails WeatherDetails { get; set; }
+
+		public City() {
+			CountryDetails = new CountryDetails();
+			WeatherDetails = new WeatherDetails();
+		}
+	}
+
+	public class CityModel
+	{
+		[Required, MaxLength(50)]
+		public string Name { get; set; }
+		[Required, MaxLength(200)]
+		public string Description { get; set; }
+		[Required, MaxLength(50)]
+		public string State { get; set; }
+		[Required, MaxLength(50)]
+		public string Country { get; set; }
+		[Required, Range(1, 5)]
+		public int Rating { get; set; }
+		[Required, DataType(DataType.Date)]
+		public DateTime EstablishedOn { get; set; }
+		[Required]
+		public long Population { get; set; }
+		[Required, MaxLength(2)]
+		public string CountryCode2 { get; set; }
+		[Required, MaxLength(3)]
+		public string CountryCode3 { get; set; }
+		[Required, MaxLength(20)]
+		public string Currency { get; set; }
+	}
+}
